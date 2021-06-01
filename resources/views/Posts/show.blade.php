c17c469227481ffa3a4af26d4677f392f5db47fe
Gabriella Di Gregorio <gabriellag@theforesight.co.uk>
2021-06-01 12:24:30 +0100

    Implemented Comments System

--- resources/views/Posts/show.blade.php
+++ resources/views/Posts/show.blade.php
@@ -10,6 +10,33 @@
 
  			<p>{{ $post->body }}</p>
 
-       	</div><!-- /.blog-main -->
+      <hr>
+      <div class="comments">
+        <ul class="list-group">
+          @foreach ($post->comments as $comment)
+            <li class="list-group-item">
+              <strong> {{ $comment->created_at->diffForHumans() }}:<br></strong>
+              {{ $comment->body }}
+            </li>
+          @endforeach
+        </ul>
+      </div>
+
+      <div class="card">
+        <div class="card-block">
+          <form method="POST" action="/posts/{{ $post->id }}/comments">
+            {{ csrf_field() }}
+            <div class="form-group">
+              <textarea name="body" placeholder="Your comment here" class="form-control" required></textarea>
+            </div>
+            <div class="form-group">
+              <button type="submit" class="btn btn-primary">Post Comment</button>
+            </div>
+          </form>
+          @include('Partials.errors')
+        </div>
+      </div>
+
+  </div><!-- /.blog-main -->
 
 @endsection
